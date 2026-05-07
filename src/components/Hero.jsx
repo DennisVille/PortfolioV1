2d5a8faf8745700cf700978729f2f0f2f2111d1b
dennis mukuria <denismukuria@gmail.com>
2026-05-08 02:15:32 +0300

    Create folder structure

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import '../styles/hero.css';
+
+function Hero(){
+    return(
+        <div className="herosection">
+            <div className="heroImage">
+            </div>
+            <div className="intro">
+                <h1>Hi, am Dennis</h1>
+                <p>Junior Full-Stack Developer passionate about building scalable and responsive web applications. Turning ideas into real, usable products.</p>
+            </div>
+        </div>
+    );
+}
+export default Hero;
